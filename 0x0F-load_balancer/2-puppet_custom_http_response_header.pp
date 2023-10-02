f6e8b23c4c9d9bda7d91751d0f1ae6d158a28e6a
Shallom Kanyori <shallomkanyori@gmail.com>
2023-10-02 14:02:10 +0000

    Remove extra configurations

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,20 +6,6 @@ package {'nginx':
   provider => 'apt',
 }
 
-# Landing page
-file {'/var/www/html/index.html':
-  ensure  => file,
-  content => "Hello World!\n",
-  require => Package['nginx'],
-  mode    => '0755',
-}
-
-# Nginx service
-service {'nginx':
-  ensure    => running,
-  subscribe => Exec['default.conf'],
-}
-
 # Configure server
 $config_cmd = 'cat << EOF > /etc/nginx/sites-available/default
 # Defualt server configuration
@@ -34,10 +20,6 @@ server {
 
   add_header X-Served-By $(hostname);
 	
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=-BE6GyHcASE;
-	}
-	
 	location / {
 		try_files \$uri \$uri/ =404;
 	}
@@ -45,8 +27,15 @@ server {
 EOF
 '
 
-exec {'default.conf':
+exec {'conf':
   command => $config_cmd,
   path    => '/usr/bin:/usr/sbin:/bin',
   require => Package['nginx'],
+}
+
+# Nginx service
+exec {'nginx_restart':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  after   =>  Exec['conf'],
 }
