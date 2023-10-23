df94ba41822734167a33a40edf3d9c9c3a48b6c3
Shallom Kanyori <shallomkanyori@gmail.com>
2023-10-23 12:25:40 +0000

    Add Python script that export API data to CSV
    Add a Python script that retrieves task data of the given employee and
    exports it into a CSV file.

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Returns information about an employee's TODO list given their ID.
+
+    Retrieves data from https://jsonplaceholder.typicode.com/.
+"""
+import requests
+import sys
+
+
+def get_url(url):
+    """Returns the json response of a call to the API.
+
+    Args:
+        url: the url to request.
+    """
+    res = requests.get(url)
+    return res.json()
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    user_url = "{}/users/{}".format(base_url, user_id)
+    user = get_url(user_url)
+
+    task_url = "{}/users/{}/todos".format(base_url, user_id)
+    tasks = get_url(task_url)
+
+    filename = "{}.csv".format(user_id)
+    with open(filename, "w", encoding="utf-8") as f:
+        for task in tasks:
+            line = '"{}","{}","{}","{}"\n'.format(user_id,
+                                                  user.get('username'),
+                                                  task.get('completed'),
+                                                  task.get('title'))
+            f.write(line)
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -11,3 +11,10 @@ Requirements:
 		- `NUMBER_OF_DONE_TASKS`: number of completed tasks
 		- `TOTAL_NUMBER_OF_TASKS`: total number of tasks, which is the sum of completed and non-completed tasks
 	- Second and N next lines display the title of completed tasks: `TASK_TITLE` (with 1 tabulation and 1 space before the `TASK_TITLE`)
+
+#### Task 1
+[1-export_to_CSV.py](1-export_to_CSV.py) is Python script that extends [0-gather_data_from_an_API.py](0-gather_data_from_an_API.py) to export data in the CSV format.
+Requirements:
+- Records all tasks that are owned by this employee
+- Format: `"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"`
+- File name: `USER_ID.csv`
