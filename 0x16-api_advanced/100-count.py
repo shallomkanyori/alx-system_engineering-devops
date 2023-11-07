649ed9cffca2d64cd01475ee2cb0dfd5982c2dc8
Shallom Kanyori <shallomkanyori@gmail.com>
2023-11-07 17:26:24 +0000

    Sort word count in descending count order

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,10 +3,12 @@
 import requests
 
 
-def count_words_rec(subreddit, word_list, after="", count=0, word_count={}):
+def count_words(subreddit, wordlist, after="", count=0, word_count={}):
     """Recursively queries Reddit API, parses all hot posts' titles for a
     given subreddit and prints sorted word frequency of given keywords.
     """
+
+    word_list = [w.lower() for w in wordlist]
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
@@ -21,37 +23,27 @@ def count_words_rec(subreddit, word_list, after="", count=0, word_count={}):
     res = requests.get(url, headers=headers, params=payload,
                        allow_redirects=False)
 
-    if res.status_code != 200:
-        return None
+    try:
+        if res.status_code == 404:
+            raise Exception
+        res = res.json()
+    except Exception:
+        return
 
-    res = res.json().get("data")
+    res = res.get("data")
     after = res.get("after")
     count = res.get("count")
     for p in res.get("children"):
-        title = p.get("data").get("title").lower()
-        for w in title.split():
+        title = p.get("data").get("title").lower().split()
+        for w in title:
             if w in word_list:
                 c = word_count.get(w, 0)
                 word_count[w] = c + 1
 
     if after is not None:
-        return count_words_rec(subreddit, word_list, after, count, word_count)
+        count_words(subreddit, wordlist, after, count, word_count)
+    else:
+        word_count = sorted(word_count.items(), key=lambda i: (-i[1], i[0]))
 
-    return word_count
-
-
-def count_words(subreddit, wordlist):
-    """Queries Reddit API, parses all hot posts' titles for a given subreddit
-    and prints sorted word frequency of given keywords.
-    """
-    word_list = {w.lower() for w in wordlist}
-
-    word_count = count_words_rec(subreddit, word_list)
-    if word_count is None:
-        return
-
-    word_count = {e[0]: e[1] for e in sorted(word_count.items(),
-                                             key=lambda i: (i[1], i[0]))}
-
-    for key, val in word_count.items():
-        print("{}: {}".format(key, val))
+        for key, val in word_count:
+            print("{}: {}".format(key, val))
