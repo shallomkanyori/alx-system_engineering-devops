19cb18fe63e45b9572492894d18ba3690182721b
Shallom Kanyori <shallomkanyori@gmail.com>
2023-11-15 09:33:29 +0000

    Fix typo in parameter name

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Fix nginx to have no failed requests while stress testing
 exec {'fix-for-nginx':
-  cmd      => 'sudo sed -i "s/15/4096/" /etc/default/nginx',
+  command  => 'sudo sed -i "s/15/4096/" /etc/default/nginx',
   provider => shell,
 }
