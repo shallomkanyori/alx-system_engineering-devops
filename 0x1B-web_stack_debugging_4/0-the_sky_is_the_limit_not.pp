3b31786e37451574b8d5490bf08d06e0c854a15b
Shallom Kanyori <shallomkanyori@gmail.com>
2023-11-15 09:38:04 +0000

    Add command to restart nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Fix nginx to have no failed requests while stress testing
 exec {'fix-for-nginx':
-  command  => 'sudo sed -i "s/15/4096/" /etc/default/nginx',
+  command  => 'sudo sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
   provider => shell,
 }
