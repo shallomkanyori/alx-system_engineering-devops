b27f4257069ce2225538bb734fcdb17c44a320b1
Shallom Kanyori <shallomkanyori@gmail.com>
2023-11-15 13:29:46 +0000

    Fix sed command to modify the file

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # Change OS open file limit configuration for the user holberton
 exec {'change-os-configuration-for-holberton-user':
-  command  => 'sudo sed "s/\(holberton[^0-9]*\)[0-9]*/\11000/" /etc/security/limits.conf && sysctl -p',
+  command  => 'sudo sed -i "s/\(holberton[^0-9]*\)[0-9]*/\11000/" /etc/security/limits.conf && sysctl -p',
   provider => shell,
 }
